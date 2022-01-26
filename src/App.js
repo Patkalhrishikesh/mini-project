85cdf0fbf483f897433f255c75fdb6b29531d882
Patkalhrishikesh <patkalhrishikesh16@gmail.com>
2022-01-26 15:07:36 +0530

    commit before subt

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 export default function App() {
+  let preloginlist
+  let location = useLocation();
+
+  return (
+    <>
+      <div>
+        <Link to="/">Login page</Link>
+        <Link to="/Register">Register page</Link>
+      </div>
+
+      <div>
+        <Link to="/welcome-home">home |</Link>
+        <Link to="/welcome-home">exploerr |</Link>
+        <Link to="/welcome-home">message |</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/welcome-home" element={<Welcome />} />
+      </Routes>
+    </>
+  );
+}
+
+function Login() {
+  let navigate = useNavigate();
+  const authantication = () => {
+    navigate("/welcome-home");
+  };
+
   return (
     <div>
-      <Router>
-        <Link to="/">page1 |</Link>
-        <Link to="/page2">page2 |</Link>
-        <Routes>
-          <Route path="/" element={<Page1 />}></Route>
-
-          <Route path="/page2" element={<Page2 />}></Route>
-        </Routes>
-      </Router>
+      <h1>login page</h1>
+      <div>
+        <input type="text" placeholder="enter Username" />
+      </div>
+      <div>
+        <input type="password" placeholder="enter password" />
+      </div>
+      <div>
+        <input type="button" value="login" onClick={authantication} />
+      </div>
     </div>
   );
 }
 
-function Page1() {
+function Register() {
   return (
     <div>
-      <h1>page1</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
-        quas similique quis, sequi ab quos suscipit! Culpa magnam esse eum?
-      </p>
+      <h1>Register page........</h1>
     </div>
   );
 }
 
-function Page2() {
+function Welcome() {
+ 
+  let navigate = useNavigate();
+  console.log("locaton");
+
   return (
     <div>
-      <h1>page2</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
-        quas similique quis, sequi ab quos suscipit! Culpa magnam esse eum?
-      </p>
+      <h1>welcome home</h1>
     </div>
   );
 }
