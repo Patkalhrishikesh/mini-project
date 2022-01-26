c4eef9dbbb3f8262d585b3287c499d8210935919
Patkalhrishikesh <patkalhrishikesh16@gmail.com>
2022-01-26 15:48:32 +0530

    b11 condition

--- src/App.js
+++ src/App.js
@@ -1,92 +1,16 @@
 import { useState } from "react";
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Routes,
-  useNavigate,
-  useParams,
-  useLocation,
-} from "react-router-dom";
 
 export default function App() {
-  let preLoginList = ["/", "/login", "/register"];
-  let location = useLocation();
-
-  return (
-    <>
-      {/** Link is Secondary */}
-
-      {preLoginList.includes(location.pathname) && (
-        <div>
-          <Link to="/login">Login | </Link>
-          <Link to="/register">Register |</Link>
-        </div>
-      )}
-
-      {!preLoginList.includes(location.pathname) && (
-        <div>
-          <Link to="/welcome-home">Home | </Link>
-          <Link to="/welcome-home">Expore | </Link>
-          <Link to="/welcome-home">Messages </Link>
-        </div>
-      )}
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route path="/welcome-home" element={<WelcomeHome />} />
-        <Route path="/explore" element={<WelcomeHome />} />
-        <Route path="/messages" element={<WelcomeHome />} />
-      </Routes>
-    </>
-  );
-}
-
-function Login() {
-  // SPECIAL FUNCTION :: HOOKS :: use...
-  let navigate = useNavigate();
-
-  const authenticate = () => {
-    // ....
-    navigate("/welcome-home");
-  };
-
+  let [active] = useState(true);
   return (
     <div>
-      <h1>Login Page</h1>
-      <div>
-        <div>
-          <input type="text" placeholder="Enter Username" />
-        </div>
-        <div>
-          <input type="password" placeholder="Enter Password" />
-        </div>
-        <div>
-          <input type="button" value="Login to App" onClick={authenticate} />
-        </div>
-      </div>
-    </div>
-  );
-}
+      <h1>Conditional exicution</h1>
 
-function Register() {
-  return <div>Reigster Page</div>;
-}
+      {active && <h3>Good morning</h3>}
 
-function WelcomeHome() {
-  let navigate = useNavigate();
+      {active && <h3>Good morning</h3>}
 
-  const signOut = () => {
-    navigate("/login");
-  };
-
-  return (
-    <div>
-      <h1>Welcome home</h1>
-      <button onClick={signOut}>Sign Out</button>
+      {active && <h3>Good morning</h3>}
     </div>
   );
 }
